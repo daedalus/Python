a4311d191fb27b7837bbfceac4130ac42e20f9b0
Darío Clavijo <clavijodario@gmail.com>
2024-02-10 13:57:20 -0300

    Update maths: add pisano period

---
+++ maths/pisano_period_sequence.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def pisano_period(m):
+    """
+    A001175
+    """
+    if m == 1:
+        return 1
+    a = b = 1
+    n = i = 0
+    while i <= (m**2):
+        b, a = a, (a + b) % m
+        if a == 1 and b == 0:
+            return n + 2
+        n += 1
+
+
+if __name__ == "__main__":
+    for i in range(1, 100):
+        print((pisano_period(i)))
